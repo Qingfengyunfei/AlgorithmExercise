65bce9a57fd5287b8c6808ed7d639b9fd15f8b45
qingfengyunfei16 <1105681132@qq.com>
2019-05-27 22:54:17 +0800

    求众数

--- src/com/leetcode/MajorityElement.java
+++ src/com/leetcode/MajorityElement.java
@@ -1,8 +1,39 @@
 package com.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在众数。
+ */
 public class MajorityElement {
+    //自己实现版本
     public int majorityElement(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+        int result = 0;
+        if(nums.length == 1){
+            return nums[0];
+        }
+        for(int num : nums){
+            if(map.containsKey(num)){
+                if(map.get(num) == nums.length/2){
+                    result = num;
+                }
+                map.put(num,map.get(num)+1);
+            }else {
+                map.put(num,1);
+            }
+        }
+        return result;
+    }
 
+    //最优解法
 
+    public int majorityElementBest(int[] nums){
+        Arrays.sort(nums);           //排序之后  直接取中间的数
+        return nums[nums.length/2];
     }
 }
