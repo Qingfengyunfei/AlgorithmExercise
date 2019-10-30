9c6ee5cd775457088b9d803aecdaa2cb8981a570
qingfengyunfei16 <1105681132@qq.com>
2019-10-31 01:49:59 +0800

    #1 两数之和

---
+++ src/com/FirstLeetcode/Array/Easy/TwoSum_1.java
@@ -0,0 +1,18 @@
+package com.FirstLeetcode.Array.Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum_1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i =0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
