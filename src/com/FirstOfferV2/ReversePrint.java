13fce307eafdd1491f0a05d7af6ffa2d99495bd8
e <1105681132@qq.com>
2020-05-26 00:18:36 +0800

    增加时间复杂度

--- src/com/FirstOfferV2/ReversePrint.java
+++ src/com/FirstOfferV2/ReversePrint.java
@@ -1,7 +1,6 @@
 package com.FirstOfferV2;
 
 import java.util.Stack;
-import java.util.function.Predicate;
 
 /**
  * 面试题06. 从尾到头打印链表
@@ -20,6 +19,12 @@ import java.util.function.Predicate;
  * 0 <= 链表长度 <= 10000
  */
 public class ReversePrint {
+
+    /**
+     * 复杂度分析：
+     * 时间复杂度 O(N)： 入栈和出栈共使用 O(N) 时间。
+     * 空间复杂度 O(N)： 辅助栈 stack 和数组 res共使用 O(N)的额外空间
+     */
     public class ListNode {
         int val;
         ListNode next;
@@ -34,10 +39,10 @@ public class ReversePrint {
             st.push(head.val);
             head = head.next;
         }
-        int[] re = new int[st.size()];
+        int[] res = new int[st.size()];
         for(int i = 0;i<st.size();i++){
-            re[i] = st.pop();
+            res[i] = st.pop();
         }
-        return re;
+        return res;
     }
 }
